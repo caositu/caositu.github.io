8482a6215645335a1444c6088da86689eb2a2128
andytaoli <litao558899@126.com>
2018-05-27 01:10:43 +0800

    particle onclick

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded",function(){new SweetScroll({});part
 	interactivity:
 {
 	detect_on:"canvas",
-	events:{onhover:{enable:!0,mode:"repulse"},onclick:{enable:!0,mode:"push"},resize:!0},
+	events:{onhover:{enable:!0,mode:"repulse"},onclick:{enable:!1,mode:"push"},resize:!0},
 	modes:{
 		grab:{distance:400,line_linked:{opacity:1}},
 		bubble:{distance:400,size:40,duration:2,opacity:8,speed:3},
