8003897323574d58c4780b5db3cff66345442aac
andytaoli <litao558899@126.com>
2018-05-27 01:23:36 +0800

    3

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded",function(){new SweetScroll({});part
 	interactivity:
 {
 	detect_on:"canvas",
-	events:{onhover:{enable:!0,mode:"bubble"},onclick:{enable:!0,mode:"push"},resize:!0},
+	events:{onhover:{enable:!0,mode:"push"},onclick:{enable:!0,mode:"push"},resize:!0},
 	modes:{
 		grab:{distance:400,line_linked:{opacity:1}},
 		bubble:{distance:400,size:40,duration:2,opacity:8,speed:3},
